748478d6326464ddfde88b3b16830c24be8f6146
J <jjout14@gmail.com>
2023-05-03 15:44:59 -0400

    Updates

---
+++ precache-manifest.ea1e0bdebea23b095bfef2b2383af20d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11c4ece9e80a89246492",
+    "url": "/date-night-randomizer/static/css/main.bf484196.chunk.css"
+  },
+  {
+    "revision": "11c4ece9e80a89246492",
+    "url": "/date-night-randomizer/static/js/main.f379f9c9.chunk.js"
+  },
+  {
+    "revision": "87117d26cfdef85cdfd8",
+    "url": "/date-night-randomizer/static/js/runtime~main.53e833ff.js"
+  },
+  {
+    "revision": "fead4868aef64d067892",
+    "url": "/date-night-randomizer/static/js/2.19fad602.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/date-night-randomizer/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "3d92ab192d38fba7a0bdaafbec995f59",
+    "url": "/date-night-randomizer/index.html"
+  }
+];
